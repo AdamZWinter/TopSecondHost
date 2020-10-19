46ad69a987c16d77e8a4cb1362ed3739e65b971a
AdamZWinter <adam@bootstrapmusic.com>
2020-10-19 22:55:00 +0000

    Updated configure.sh to topsecondhost

--- configure.sh
+++ configure.sh
@@ -2,13 +2,13 @@
 
 #Enter your domain name (suffix).  For example,  example.com  (without the host portion)
 #You must have an A record set and propagated already.  Check at dnschecker.org
-suffix='hostsecondtop.com'
+suffix='topsecondhost.com'
 
 #Enter the username your site will use to access the database
 #This is the service account used by the site to access the database
 #This user name cannot be longer than 32 characters
 #You can leave the default value here
-dbuser='hostsecondtop'
+dbuser='topsecondhost'
 
 #Enter the database password that your site will use
 #This is not the root password for the database
@@ -20,8 +20,8 @@ dbuser='hostsecondtop'
 dbpw='putthepasswordhereinsidethesinglequotes'
 
 #Enter a name for your site database
-#For example, if your site is host.second.top, you could put "secondtop" here
-database='secondtop'
+#For example, if your site is host.second.top, you could put "secondtop" or "second" here
+database='topsecond'
 
 workingdir=$(pwd)
 
