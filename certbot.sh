a7ad9491b2cb21381c95ae053178d16c1d4ea5a7
AdamZWinter <adam@bootstrapmusic.com>
2020-10-20 23:16:16 +0000

    Move ssl.conf to zzzz-ssl.conf

--- certbot.sh
+++ certbot.sh
@@ -7,6 +7,8 @@ wget https://dl.eff.org/certbot-auto
 chmod a+x certbot-auto
 sudo ./certbot-auto --apache --debug
 
+sudo mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/zzzz-ssl.conf
+
 sudo service httpd restart
 sudo ls -l /etc/httpd/conf.d/
 
