2350cf25306ebaa172038516aef49c97a15d9af0
AdamZWinter <adam@bootstrapmusic.com>
2020-10-20 04:18:22 +0000

    Adding html.sh

---
+++ html.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source configure.sh
+
+cd html
+cp -R * /var/www/html
+
+sudo sed -i "s/topsecondhost.com/${suffix}/g" /var/www/html/robots.txt
+
+sudo sed -i "s/topsecondhost.com/${suffix}/g" /var/www/html/sitemap.xml
+
+sudo sed -i "s/secondtopdbuser/${suffix}/g" /var/www/html/dashboard/menu.php
--- html/dashboard/menu.php
+++ html/dashboard/menu.php
@@ -3,11 +3,10 @@
 
 echo '
   <a class="bigScreen left">&nbsp;&nbsp;</a>
-  <a class="dashboardMenu left" href="https://gcbookings.com/dashboard/wizard/wizardStart.php">Wizard</a>
-  <a class="dashboardMenu left" id="settings" href="https://gcbookings.com/dashboard/settings.php">Settings</a>
-  <a class="dashboardMenu left" id="terms" href="https://gcbookings.com/dashboard/terms.php">Terms</a>
-  <a class="dashboardMenu left" id="emailMessage" href="https://gcbookings.com/dashboard/emailMessage.php">Emails</a>
-  <a class="dashboardMenu left" id="manualBooking" href="https://gcbookings.com/dashboard/manualBooking.php">Booking</a>
+  <a class="dashboardMenu left" href="https://topsecondhost.com/dashboard/wizard/wizardStart.php">Wizard</a>
+  <a class="dashboardMenu left" id="settings" href="https://topsecondhost.com/dashboard/settings.php">Settings</a>
+  <a class="dashboardMenu left" id="terms" href="https://topsecondhost.com/dashboard/terms.php">Terms</a>
+  <a class="dashboardMenu left" id="emailMessage" href="https://topsecondhost.com/dashboard/emailMessage.php">Emails</a>
 ';
 
 ?>
